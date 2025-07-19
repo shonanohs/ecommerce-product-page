58012893880b8be8738899df2c0bcc6d9905efb6
Shona Fitzpatrick <71610718+shonanohs@users.noreply.github.com>
2025-07-19 15:40:52 +0100

    Added functionality to lightbox arrow buttons

--- src/script.js
+++ src/script.js
@@ -4,9 +4,13 @@ let currentImageIndex = 1; // Tracks which thumbnail is selected (1–4)
 const quantityText = document.getElementById("quantity-text");
 const productImgMain = document.getElementById("product-img-main");
 const thumbnails = document.querySelectorAll(".product-thumbnail");
+const lightboxImg = document.getElementById("lightbox-img");
 
 document.getElementById("plus-btn").addEventListener("click", () => updateQuantity("plus"));
 document.getElementById("minus-btn").addEventListener("click", () => updateQuantity("minus"));
+
+document.getElementById("next-btn").addEventListener("click", () => updateLightboxImage("next"));
+document.getElementById("prev-btn").addEventListener("click", () => updateLightboxImage("prev"));
 
 // Set up thumbnail click listeners dynamically
 thumbnails.forEach((thumb, index) => {
@@ -44,12 +48,21 @@ function updateMainProductImg(index) {
 
 // Add styling to selected thumbnail
 function addThumbnailStyleOnClick(selectedThumbnail) {
-  thumbnails.forEach(t => t.classList.remove("product-thumbnail-clicked"));
+  thumbnails.forEach(thumb => thumb.classList.remove("product-thumbnail-clicked"));
   selectedThumbnail.classList.add("product-thumbnail-clicked");
 }
 
 // Function for syncing the lightbox image
 function setLightboxImage(index) {
-  const lightboxImg = document.getElementById("lightbox-img");
   lightboxImg.src = `./images/image-product-${index}.jpg`;
+}
+
+// Update lightbox image when arrow buttons clicked
+function updateLightboxImage(direction) {
+    if (direction === "next") {
+        currentImageIndex = currentImageIndex === 4 ? 1 : currentImageIndex + 1;
+    } else if (direction === "prev") {
+        currentImageIndex = currentImageIndex === 1 ? 4 : currentImageIndex - 1;
+    }
+    setLightboxImage(currentImageIndex);
 }
